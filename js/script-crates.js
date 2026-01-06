26cf24a70709249cdb18328b71ca1b66854010fe
unkv0wn <cjeanb0@gmail.com>
2026-01-06 11:46:16 -0300

    [WIP] - Feature em teste3

--- js/script-crates.js
+++ js/script-crates.js
@@ -436,7 +436,7 @@ function returnFiles() {
       HideItemFlags: true
       Commands:
       - 'aegive %player% ${(itensDicionario[inputItemType]).toLowerCase()} 1 custommodeldata:${inputItemCustomModelData} name:${(convertToBracketedRGB(RgbItemName)).replace(/ /g, '_')} lore:&fਦ_&e%essentials_nickname%|&f|&f${inputSkinUnicode}|&f'
-      - 'broadcast &f⨏ &7%player% encontrou ${RgbItemName}&7! na ${inputRgbCrate}&7!'`;
+      - 'broadcast &f⨏ &7%player% encontrou ${RgbItemName}&7! &r&7na ${inputRgbCrate}&7!'`;
 
   // Atualiza o campo de texto com a configuração final
   if (document.getElementById('etapa-criacao').value == "config") {
